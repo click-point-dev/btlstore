979b7f0ddc4fd5e185ea40c150d8cba8cc0d9023
Roman <romankrets@gmail.com>
2024-02-11 00:33:51 +0700

    implement card-circle

--- src/widgets/cardCircle/cardCircle.ts
+++ src/widgets/cardCircle/cardCircle.ts
@@ -1,3 +1,69 @@
-export function cardCircle(): null {
-	return null;
+import gsap from 'gsap';
+
+export function cardCircle(): void {
+   const circles: Element[] = gsap.utils.toArray('.card-circle');
+   if (!circles.length) return;
+
+   circles.forEach(circle => {
+      const hoverAnimation = gsap.timeline({ paused: true });
+
+      const image = circle.querySelector('.card-circle__image > img');
+      const gradient = circle.querySelector('.card-circle__body');
+      const description = circle.querySelector('.card-circle__description');
+      const title = circle.querySelector('.card-circle__title');
+
+      hoverAnimation
+         .to(gradient, {
+            backgroundImage:
+               'linear-gradient(0deg, #0055b0 3.03%, #0055b000 50%)',
+         })
+         .to(
+            image,
+            {
+               duration: 0.3,
+               css: {
+                  filter: 'grayscale(0)',
+               },
+            },
+            0,
+         )
+         .to(title, { y: 0 }, '-=0.2')
+         .to(
+            description,
+            { y: 0, ease: 'back.out(0.7)', duration: 0.7 },
+            '-=0.4',
+         );
+
+      function handleHoverAnimationStart() {
+         hoverAnimation.play();
+      }
+
+      function handleHoverAnimationRevers() {
+         const isActive = circle.classList.contains('swiper-slide-active');
+         if (isActive) return;
+         hoverAnimation.reverse();
+      }
+
+      function handleObserver(mutationRecord: MutationRecord[]) {
+         const target = mutationRecord[0].target as Element;
+         const isActive = target.classList.contains('swiper-slide-active');
+
+         if (isActive) {
+            handleHoverAnimationStart();
+         }
+         if (!isActive) {
+            handleHoverAnimationRevers();
+         }
+      }
+
+      const configObserver = { attributeFilter: ['class'], subtree: true };
+      let observer = new MutationObserver(mutationRecord =>
+         handleObserver(mutationRecord),
+      );
+
+      observer.observe(circle, configObserver);
+
+      circle.addEventListener('mouseover', handleHoverAnimationStart);
+      circle.addEventListener('mouseout', handleHoverAnimationRevers);
+   });
 }
