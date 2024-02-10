473a86736d903bc6b9b589d6cec4694173328d43
Roman <romankrets@gmail.com>
2024-02-11 01:46:51 +0700

    update slider cases styles

--- src/widgets/sliderCases/sliderCases.ts
+++ src/widgets/sliderCases/sliderCases.ts
@@ -5,7 +5,7 @@ export function sliderCases(): void {
    const sliderCases = new Swiper('.card-circle__swiper', {
       spaceBetween: 30,
       speed: 500,
-      initialSlide: 1,
+      initialSlide: 0,
       slidesPerView: 'auto',
       slideToClickedSlide: true,
       centeredSlides: true,
