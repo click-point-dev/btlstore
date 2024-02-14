44347776e5c82ff7c6e2df5a16aef11841a2b1bc
Roman <romankrets@gmail.com>
2024-02-14 15:33:30 +0700

    try to fix sliders params and cases

--- src/widgets/sliderCases/sliderCases.ts
+++ src/widgets/sliderCases/sliderCases.ts
@@ -2,21 +2,30 @@ import Swiper from 'swiper';
 // import 'swiper/css';
 
 export function sliderCases(): void {
-   const sliderCases = new Swiper('.card-circle__swiper', {
-      spaceBetween: 30,
-      speed: 500,
-      initialSlide: 0,
-      slidesPerView: 'auto',
-      slideToClickedSlide: true,
-      centeredSlides: true,
-      loop: true,
-      breakpoints: {
-         360: {
-            spaceBetween: 10,
+   const cases: HTMLElement[] = Array.from(
+      document.querySelectorAll('.card-circle__swiper'),
+   );
+
+   if (!cases.length) return;
+
+   cases.forEach(item => {
+      const sliderCases = new Swiper(item, {
+         spaceBetween: 30,
+         speed: 500,
+         initialSlide: 0,
+         slidesPerView: 'auto',
+         slideToClickedSlide: true,
+         centeredSlides: true,
+         loop: true,
+         loopAdditionalSlides: 1,
+         breakpoints: {
+            360: {
+               spaceBetween: 10,
+            },
+            475: {
+               spaceBetween: 30,
+            },
          },
-         475: {
-            spaceBetween: 30,
-         },
-      },
+      });
    });
 }
--- src/widgets/sliderParams/sliderParams.ts
+++ src/widgets/sliderParams/sliderParams.ts
@@ -1,13 +1,29 @@
 import Swiper from 'swiper';
 
 export function sliderParams(): void {
-   const sliderParams = new Swiper('.slider-params', {
-      spaceBetween: 30,
-      speed: 500,
-      initialSlide: 1,
-      slidesPerView: 'auto',
-      slideToClickedSlide: true,
-      centeredSlides: true,
-      loop: true,
+   const params: HTMLElement[] = Array.from(
+      document.querySelectorAll('.slider-params'),
+   );
+
+   if (!params.length) return;
+
+   params.forEach(item => {
+      const sliderParams = new Swiper(item, {
+         speed: 500,
+         initialSlide: 0,
+         slidesPerView: 'auto',
+         slideToClickedSlide: true,
+         centeredSlides: true,
+         loopAdditionalSlides: 1,
+         loop: true,
+         breakpoints: {
+            360: {
+               spaceBetween: 10,
+            },
+            475: {
+               spaceBetween: 30,
+            },
+         },
+      });
    });
 }
