7b7ce1dceb8a612c85ce95c615a347d495ac4b31
Roman <romankrets@gmail.com>
2024-02-14 17:05:34 +0700

    page vacancies init

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ export * from './mainPage';
 export * from './servicesPage';
 export * from './contactsPage';
 export * from './aboutPage';
+export * from './vacanciesPage';
---
+++ src/pages/vacanciesPage/vacanciesPage.ts
@@ -0,0 +1,3 @@
+export function vacanciesPage(): void {
+
+}
