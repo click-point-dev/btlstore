a1b506f6583e54e2c466c19bdf9765c83f8fb381
Roman <romankrets@gmail.com>
2024-03-04 11:30:30 +0700

    update foo

--- src/shared/libs/documentLock/documentLock.ts
+++ src/shared/libs/documentLock/documentLock.ts
@@ -1,22 +1,13 @@
 export function documentLock(): void {
    const element = document.documentElement;
-
    if (!element) return;
 
-   if (!element.classList.contains('lock')) {
-      element.classList.add('lock');
-      element.classList.contains('unlock') &&
-         element.classList.remove('unlock');
-   }
+   !element.classList.contains('lock') && element.classList.add('lock');
 }
 
 export function documentUnlock(): void {
    const element = document.documentElement;
-
    if (!element) return;
 
-   if (element.classList.contains('lock')) {
-      element.classList.remove('lock');
-      !element.classList.contains('unlock') && element.classList.add('unlock');
-   }
+   element.classList.contains('lock') && element.classList.remove('lock');
 }
