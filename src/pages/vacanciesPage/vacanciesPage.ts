f0b2f901225e726236330d16d6ba11e3bd5eff52
Roman <romankrets@gmail.com>
2024-03-04 11:28:23 +0700

    implement vacancy-name init and past it into popup

--- src/pages/vacanciesPage/vacanciesPage.ts
+++ src/pages/vacanciesPage/vacanciesPage.ts
@@ -1,3 +1,21 @@
-export function vacanciesPage(): void {
+import { insertContent } from '../../shared';
 
+export function vacanciesPage(): void {
+   const ctaButtons: HTMLElement[] = Array.from(
+      document.querySelectorAll('.vacancy__cta [data-popup-type]'),
+   );
+
+   if (!ctaButtons.length) return;
+
+   ctaButtons.forEach(button => {
+      const vacancyName = button
+         .closest('.vacancy__body')
+         .querySelector('.vacancy__name').textContent;
+
+      const placeholder: HTMLElement = document.querySelector(
+         '.full-width-form__vacancy-title',
+      );
+
+      button.onclick = () => insertContent(vacancyName, placeholder);
+   });
 }
