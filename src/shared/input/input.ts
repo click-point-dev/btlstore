728090f52b09c3dbd4a873152567dd6d9d2748a6
Roman <romankrets@gmail.com>
2024-03-04 11:24:04 +0700

    update animations

--- src/shared/input/input.ts
+++ src/shared/input/input.ts
@@ -4,20 +4,20 @@ export function input(): void {
    const inputs: HTMLInputElement[] = gsap.utils.toArray('.input');
    if (!inputs.length) return;
 
-   inputs.map(input => {
+   inputs.forEach(input => {
       const parent = input.closest('.input-block'),
          label = parent.querySelector('label');
 
       const animationOnFocus = gsap.timeline({ paused: true });
       const animationOnHover = gsap.timeline({ paused: true });
 
-      animationOnFocus
-         .to(label, {
-            bottom: '100%',
-            // ease: 'sine.in',
-            duration: 0.2,
-         })
-         .to(label, { fontSize: 13, duration: 0.2 }, 0);
+      animationOnFocus.to(label, {
+         transformOrigin: 'top left',
+         top: '-=42px',
+         ease: 'power2.inOut',
+         scale: 0.55,
+         duration: 0.3,
+      });
 
       animationOnHover.to(label, { y: 5, duration: 0.2 });
 
