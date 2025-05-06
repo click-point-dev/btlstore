e92078799b4dec52b92ff4786da9d5756c864499
novel86 <sir.novel86@yandex.ru>
2025-05-06 15:02:34 +0700

    implement paint vacancies count

--- src/pages/vacanciesPage/vacanciesPage.ts
+++ src/pages/vacanciesPage/vacanciesPage.ts
@@ -8,25 +8,44 @@ export function vacanciesPage(): void {
          '.vacancy__cta [data-popup-type="popup-vacancy"]',
       ),
    );
+   const vacanciesCountPlaceholder = Array.from(
+      document.querySelectorAll('[data-vacancy-count-placeholder]'),
+   );
 
-   if (!ctaButtons.length) return;
+   function paintVacancyTitle(triggers: HTMLElement[]) {
+      triggers.forEach(button => {
+         const vacancyElement: HTMLElement = button.closest(
+            '[data-vacancy-title]',
+         );
+         const vacancyTitle = vacancyElement.dataset.vacancyTitle.trim();
 
-   ctaButtons.forEach(button => {
-      const vacancyElement: HTMLElement = button.closest(
-         '[data-vacancy-title]',
-      );
-      const vacancyTitle = vacancyElement.dataset.vacancyTitle.trim();
+         const vacancyForm: HTMLFormElement =
+            document.querySelector('[data-vacancy]');
 
-      const vacancyForm: HTMLFormElement =
-         document.querySelector('[data-vacancy]');
+         const placeholder: HTMLElement = document.querySelector(
+            '[data-vacancy-title-placeholder]',
+         );
 
-      const placeholder: HTMLElement = document.querySelector(
-         '[data-vacancy-title-placeholder]',
-      );
+         button.onclick = () => {
+            insertContent(vacancyTitle, placeholder);
+            vacancyForm.dataset.vacancy = vacancyTitle;
+         };
+      });
+   }
 
-      button.onclick = () => {
-         insertContent(vacancyTitle, placeholder);
-         vacancyForm.dataset.vacancy = vacancyTitle;
-      };
-   });
+   function paintVacanciesCount(placeholder: Element[]) {
+      placeholder.forEach(element => {
+         const count = element
+            .closest('.vacancies__container')
+            .querySelectorAll('[data-vacancy-title]').length;
+
+         element.textContent = `${count}`;
+      });
+   }
+
+   //пэинт заголовка вакансии в форме
+   ctaButtons.length && paintVacancyTitle(ctaButtons);
+
+   // пэинт количества открытых вакансий
+   vacanciesCountPlaceholder && paintVacanciesCount(vacanciesCountPlaceholder);
 }
