5c6f423d76b7010b79e3c59e840b0d77690456ea
novel86 <143467869+click-point-dev@users.noreply.github.com>
2025-07-08 17:33:19 +0700

    update pr page with printing cases function

--- src/pages/prPage/prPage.ts
+++ src/pages/prPage/prPage.ts
@@ -1,8 +1,25 @@
-import { checkViewportWidth, isAvailableUrl } from '../../shared';
-import { sliderCases } from '../../widgets';
+import { casesData } from '../../entities';
+import { isAvailableUrl } from '../../shared';
+import { casesCardsWidget } from '../../widgets';
+import DOMPurify from 'dompurify';
 
 export function prPage(): void {
-   //    if (!isAvailableUrl('pr')) return;
+   if (!isAvailableUrl('pr')) return;
+
+   // рендер карточек в портвфолио
+   const portfolioBlock = document.querySelector(
+      '[data-portfolio]',
+   ) as HTMLDivElement;
+
+   if (portfolioBlock) {
+      const clear = DOMPurify.sanitize(
+         casesCardsWidget(casesData, false, { type: 'PR' }),
+         { USE_PROFILES: { svg: true, svgFilters: true, html: true } },
+      );
+
+      portfolioBlock.innerHTML = clear;
+   }
+
    //    // инициализация слайдера в кейсах
    //    const circlesSliders = Array.from(
    //       document.querySelectorAll('.corporate-events .slider-cases'),
