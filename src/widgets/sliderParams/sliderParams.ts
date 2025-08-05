ca17b1f03322b27ed5c7a31dbfea13d537ed7779
Roman <86733860+Novel86@users.noreply.github.com>
2025-08-05 11:07:04 +0700

    - fix swiper params

--- src/widgets/sliderParams/sliderParams.ts
+++ src/widgets/sliderParams/sliderParams.ts
@@ -1,23 +1,31 @@
 import Swiper from 'swiper';
+import { Keyboard } from 'swiper/modules';
 
 export function sliderParams(): void {
-   const params: HTMLElement[] = Array.from(
-      document.querySelectorAll('.slider-params'),
-   );
+   const params: HTMLElement[] = Array.from(document.querySelectorAll('.slider-params'));
 
    if (!params.length) return;
 
    params.forEach(item => {
+      const centralSlideNumber = Math.floor(
+         Array.from(item.querySelectorAll('.swiper-slide')).length / 2,
+      );
       const sliderParams = new Swiper(item, {
+         modules: [Keyboard],
+         spaceBetween: 30,
          speed: 500,
-         initialSlide: 1,
+         initialSlide: centralSlideNumber,
          slidesPerView: 'auto',
          slideToClickedSlide: true,
          centeredSlides: true,
+         // loop: true,
+         keyboard: {
+            enabled: true,
+         },
          breakpoints: {
             360: {
                spaceBetween: 10,
-               loop: true,
+               // loopAdditionalSlides: 1,
             },
             475: {
                spaceBetween: 30,
