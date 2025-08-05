cf1d81e7973b76695759773292dd2e4a16edcade
Roman <86733860+Novel86@users.noreply.github.com>
2025-08-05 11:00:50 +0700

    - about redisign is done

--- src/pages/aboutPage/aboutPage.ts
+++ src/pages/aboutPage/aboutPage.ts
@@ -1,3 +1,12 @@
-export function aboutPage(): void {
+import { citiesCorporateData } from '../../entities';
+import { renderCitiesList } from '../../features';
+import { isAvailableUrl } from '../../shared';
 
+export function aboutPage(): void {
+   if (!isAvailableUrl('about')) return;
+
+   // рендер списка ссылок городов
+   const citiesListPlaceholder = document.querySelector('[data-links-list]') as HTMLElement;
+   citiesListPlaceholder && renderCitiesList(citiesListPlaceholder, citiesCorporateData);
 }
+aboutPage();
